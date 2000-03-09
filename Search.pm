b70160f05b296bb0b4764f84ca4c6113cc66cb99
Martin 'Kingpin' Thurn <kingpin@dcswcc.org>
2000-03-09 16:28:43 +0000

    Import of MTHURN/Bundle-WWW-Search-1.01 from CPAN.
    gitpan-cpan-distribution: Bundle-WWW-Search
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         MTHURN/Bundle-WWW-Search-1.01.tar.gz
    gitpan-cpan-author:       MTHURN
    gitpan-cpan-maturity:     released

---
+++ Search.pm
@@ -0,0 +1,38 @@
+package Bundle::WWW::Search;
+
+$VERSION = '1.01';
+
+__END__
+
+=head1 NAME
+
+Bundle::WWW::Search - A bundle to install basic WWW::Search modules and prerequisites
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::WWW::Search'>
+
+=head1 CONTENTS
+
+Bundle::LWP		- prerequisite
+
+URI::Escape             - prerequisite
+
+HTML::Parser            - prerequisite
+
+HTML::TreeBuilder       - prerequisite
+
+WWW::Search
+
+=head1 DESCRIPTION
+
+This bundle gathers together all of the prerequisite modules for
+WWW::Search, as well as the basic WWW::Search mechanism itself.
+
+=head1 AUTHOR
+
+Martin Thurn E<lt>F<martinthurn@iname.com>E<gt>
+(Author only of this bundle, not necessarily of the modules it lists)
+
+=cut
+
